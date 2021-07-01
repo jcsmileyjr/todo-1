49fa39c1f9accf88513dcc75f21ebb5a03053f3c
Smiley <jcsmileyjr@gmail.com>
2021-07-01 12:20:27 -0500

    Styling: Update header layout positioning
    Wasn't design responsive

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 function App() {
   return (
     <article className="App">
+      <header>
+        <h1 className="header--title__style">To Do List</h1>
+      </header>
       <section id="content">
-        <header>
-          <h1 className="header--title__style">To Do List</h1>
-        </header>
       </section>
     </article>
   );
