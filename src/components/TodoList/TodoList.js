06191d7d1cf087eec0e173aea15ca718b2834b99
Smiley <jcsmileyjr@gmail.com>
2021-07-03 09:30:56 -0500

    Feature: Add animation from the react-reveal libary
    When a task is created, it's animated from the left to final resting place

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,11 +1,14 @@
 import "./todoList.css";
 import TodoItem from "../todoItem/TodoItem";
+import Fade from 'react-reveal/Fade';
 
 const TodoList = (props) => {
     return(
         <article className="todoList__layout--style">
             {props.todos.map(todo =>(
-                <TodoItem action={todo.action} editTodo={props.editTodo} complete={todo.done} todoId={todo.id} todoID={todo.id} completed={props.completed} removeTask={props.removeTask} key={todo.id} />
+                <Fade left key={todo.id}>
+                    <TodoItem action={todo.action} editTodo={props.editTodo} complete={todo.done} todoId={todo.id} todoID={todo.id} completed={props.completed} removeTask={props.removeTask} />
+                </Fade>
             ))}
         </article>
     );
