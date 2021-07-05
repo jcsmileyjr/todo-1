7b133f62711b42e9d8d0baa3dc40da10b6ceb02b
Smiley <jcsmileyjr@gmail.com>
2021-07-05 11:41:20 -0500

    Accessibility: Use can use the Enter key  to submit tasks

--- src/components/addToDo/AddToDo.js
+++ src/components/addToDo/AddToDo.js
@@ -2,10 +2,15 @@ import React from 'react';
 import "./addToDo.css";
 
 const AddToDo = (props) => {
+    const handleEnterKeyPress = (e) => {
+       if(e.charCode === 13){
+           props.onSubmit();
+       } 
+    }
     return(
         <section className="addtodo__layout--style">
-            <label for="addTodoInput" className="addtodo__label--style">Add an Item</label>
-            <input id="addTodoInput" onChange={props.newTask} value={props.value} className="addtodo__input--style" type="text" />
+            <label htmlFor="addTodoInput" className="addtodo__label--style">Add an Item</label>
+            <input id="addTodoInput" onChange={props.newTask} onKeyPress={(e)=> {handleEnterKeyPress(e)}} value={props.value} className="addtodo__input--style" type="text" />
             <div className="addtodo__button--container">
                 <button aria-label="Click to add task to to do list" onClick={props.onSubmit} className="addtodo__button--style">Add</button>
             </div>
